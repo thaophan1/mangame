1c1188a08a679654e4b43a7bfc4348c125c1acde
thaophan1 <thao_phan@ucsb.edu>
2020-12-28 16:05:00 -0800

    NavBar desktop version redesign

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,18 @@ import './NavBar.css'
 
 class NavBar extends Component {
     state = {  }
+
     render() { 
         return ( 
-            <nav>
-                <h2 className = "logo">MANGIME</h2>
-                <ul className = "nav-list">
-                    <li className = "nav-link"><a href = "#">Top Anime</a></li>
-                    <li className = "nav-link"><a href = "#">Top Manga</a></li>
-                    <li className = "nav-link"><a href = "#">Anime Recommendations</a></li>
-                    <li className = "nav-link"><a href = "#">Manga Recommendations</a></li>
+            <div className = "nav-bar">
+                <h2 className="logo">MANGIME</h2>
+                <ul className = "nav-links">
+                    <li><a href = "#">Top Anime</a></li>
+                    <li><a href = "#">Top Manga</a></li>
+                    <li><a href = "#">Anime Recommendations</a></li>
+                    <li><a href = "#">Manga Recommendations</a></li>
                 </ul>
-            </nav>
+            </div>
         );
     }
 }
