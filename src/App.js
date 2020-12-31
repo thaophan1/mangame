8b97ffd7b580071a25fba6e99400da5f96c993af
thaophan1 <thao_phan@ucsb.edu>
2020-12-31 13:48:19 -0800

    NavBar design finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import NavBar from './components/NavBar'
+import './App.css'
 
 function App() {
   return (
