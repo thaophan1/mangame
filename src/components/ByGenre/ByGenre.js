335247e38eb63c0b4861d4b84689eb9af434a33b
thaophan1 <thao_phan@ucsb.edu>
2021-03-28 13:53:45 -0700

    now able to get top anime and manga based off of selected genre

--- src/components/ByGenre/ByGenre.js
+++ src/components/ByGenre/ByGenre.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FiArrowLeftCircle } from 'react-icons/fi'
 import './ByGenre.css'
 
 class ByGenre extends Component {
@@ -8,8 +9,38 @@ class ByGenre extends Component {
         this.state = {
             topGenreAnime: [],
             topGenreManga: [],
+            chosenGenre: null,
             isLoaded: false
         }
+
+        this.genreClick = this.genreClick.bind(this)
+    }
+
+    genreClick(genre) {
+        this.setState({
+            chosenGenre: genre
+        })
+
+        fetch(`https://infinite-coast-32217.herokuapp.com/https://api.jikan.moe/v3/genre/anime/${genre.genreID}`)
+            .then(result => result.json())
+            .then((json) => {
+                this.setState({
+                    topGenreAnime: json.anime,
+                })
+
+                console.log(`Anime: ${this.state.topGenreAnime}`)
+            })
+
+        fetch(`https://infinite-coast-32217.herokuapp.com/https://api.jikan.moe/v3/genre/manga/${genre.genreID}`)
+            .then(result => result.json())
+            .then((json) => {
+                this.setState({
+                    topGenreManga: json.manga,
+                    isLoaded: true
+                })
+
+                console.log(`Manga: ${this.state.topGenreManga}`)
+            })
     }
 
     render() { 
@@ -20,7 +51,7 @@ class ByGenre extends Component {
             <div className = "heading">Get Recommendations Based on Genre</div>
             <ul className = "genre-list">
                 {genres.map((genre) => 
-                    <li key = {genre.genreID} className = "genre-button">
+                    <li key = {genre.genreID} className = "genre-button" onClick = {() => this.genreClick(genre)}>
                         {genre.genreName}
                     </li>)
                 }
